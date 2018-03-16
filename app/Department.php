66a8aa244d85e4c039147c24878685e24b8266ab
Chris Wieber <chriswieber@miad.edu>
2018-03-16 11:25:10 -0500

    Add Department Seeder

--- app/Department.php
+++ app/Department.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
+	protected $fillable = ['name'];
     public function users() {
     	return $this->belongsToMany('App\User');
     }
